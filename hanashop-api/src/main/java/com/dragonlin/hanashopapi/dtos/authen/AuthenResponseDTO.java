da099040a74ecb1e2af8e4cb52274a07052fe7c7
dragonlin22 <DragonLin.030122@gmail.com>
2022-01-05 21:29:52 +0700

    update

--- hanashop-api/src/main/java/com/dragonlin/hanashopapi/dtos/authen/AuthenResponseDTO.java
+++ hanashop-api/src/main/java/com/dragonlin/hanashopapi/dtos/authen/AuthenResponseDTO.java
@@ -5,11 +5,9 @@ import lombok.Data;
 @Data
 public class AuthenResponseDTO {
     private String id;
+    private String name;
     private String email;
-    private String avatar;
     private String phone;
-    private String github;
-    private String linkedln;
-    private String facebook;
-    private String gitlab;
+    private String address;
+    private String role;
 }
